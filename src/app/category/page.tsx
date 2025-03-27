dd748277ab74502161acb2de468714e535fa5e7a
24LP8255 24LP8255 <24LP8255@G301-CC0203-2.local>
2025-03-27 19:26:02 +0800

    fix provider

--- src/app/category/page.tsx
+++ src/app/category/page.tsx
@@ -3,13 +3,16 @@
 import MenuContainerCategory from "@/app/category/_features/CategoryPage";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import { DebounceLoadingProvider } from "@/provider/DebounceLoaderProvider copy";
 
 export default function Home() {
   return (
+     <DebounceLoadingProvider>
     <div>
       <Header />
       <MenuContainerCategory />
       <Footer />
     </div>
+    </DebounceLoadingProvider>
   );
 }
