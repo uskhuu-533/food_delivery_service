f4c8c61dac04e6d4d5d8dd30741fec7e2386e5f3
24LP8255 24LP8255 <24LP8255@G301-CC0203-2.local>
2025-03-28 17:35:17 +0800

    router

--- src/app/resetpassword/_features/NewPassword.tsx
+++ src/app/resetpassword/_features/NewPassword.tsx
@@ -1,29 +1,45 @@
 "use client";
 
+import Logo from "@/components/icons/Logo";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import axios from "axios";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { toast } from "sonner";
 
 const NewPassword = ({ email }: { email: string }) => {
+  const router = useRouter();
   const [passwords, setPasswords] = useState({
     password: "",
     confirm: "",
-    email : email
+    email: email,
   });
   const chanegPassword = async () => {
     if (passwords.confirm === passwords.password) {
-    try {
-      const response = await axios.put(
-        `https://food-backend-8ud7.onrender.com/users/password`,
-        passwords
-      );
-      if (response.status === 200) {
+      try {
+        const response = await axios.put(
+          `https://food-backend-8ud7.onrender.com/users/password`,
+          passwords
+        );
+        if (response.status === 200) {
+          toast(
+            <div className="flex text-xl text-[#EF4444] pl-10 items-center gap-5 justify-center">
+              <Logo />
+              <p>Password changed</p>
+            </div>,
+            {
+              description: "",
+              position: "top-center",
+            }
+          );
+          router.push('/login')
+        }
+      } catch (error) {
+        console.log(error);
       }
-    } catch (error) {
-      console.log(error);
     }
-  }};
+  };
   return (
     <div className="flex flex-col gap-5">
       <p>Create new password</p>
